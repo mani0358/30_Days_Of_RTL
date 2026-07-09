c1fb67c6710cc90e9447ba4067f2cfac6b985e65
Manjinder Singh <msingh60_be24@thapar.edu>
2026-07-09 16:15:18 +0530

    Add comparator module for 2-bit input comparison

---
+++ Day_03/comparator.v
@@ -0,0 +1,12 @@
+`timescale 1ns/1ps
+module comparator(
+	input [1:0] a_in,
+	input [1:0] b_in,
+	output a_gt_b,
+	output  a_lt_b,
+	output a_eq_b
+);
+assign  a_gt_b = (a_in >  b_in);
+assign  a_lt_b = ( a_in <  b_in);
+assign  a_eq_b = (a_in == b_in);
+endmodule 
