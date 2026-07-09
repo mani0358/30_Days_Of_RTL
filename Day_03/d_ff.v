4bb18a54cc2a0a237ce24e7c440e1b2a3ab8ea32
Manjinder Singh <msingh60_be24@thapar.edu>
2026-07-09 16:10:55 +0530

    Update d_ff.v

--- Day_03/d_ff.v
+++ Day_03/d_ff.v
@@ -1,1 +1,16 @@
-
+`timescale 1ns/1ps
+module d_ff(
+	input din,
+	input clk,
+	input rst,
+	output reg dout
+);
+always @(posedge clk or posedge rst )begin
+	if(rst) begin
+	dout <= 0;
+end
+else begin 
+dout <= din;
+end
+end
+endmodule
