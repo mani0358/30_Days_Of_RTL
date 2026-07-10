3b31c259f38da568d49997c783a94ba379983668
Manjinder Singh <msingh60_be24@thapar.edu>
2026-07-10 21:54:08 +0530

    Update counter.v

--- Day_04/counter.v
+++ Day_04/counter.v
@@ -1,1 +1,16 @@
 `timescale 1ns/1ps
+
+module counter(
+    input clk,
+    input rst,
+    output reg [2:0] count
+);
+
+always @(posedge clk) begin
+    if (rst)
+        count <= 3'b000;
+    else
+        count <= count + 1'b1;
+end
+
+endmodule
